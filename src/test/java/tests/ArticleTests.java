42ae90488e955852feb075e10c7f372ac3778c32
Yaremenko Roman <roman.jaremenko@gmail.com>
2022-05-27 00:00:14 +0300

    @Epic, @Features, @Severity added

--- src/test/java/tests/ArticleTests.java
+++ src/test/java/tests/ArticleTests.java
@@ -1,7 +1,6 @@
 package tests;
 
-import io.qameta.allure.Description;
-import io.qameta.allure.Step;
+import io.qameta.allure.*;
 import io.qameta.allure.junit4.DisplayName;
 import lib.CoreTestCase;
 import org.junit.Assert;
@@ -11,12 +10,15 @@ import ui.SearchPageObject;
 import ui.factories.ArticlePageObjectFactory;
 import ui.factories.SearchPageObjectFactory;
 
+@Epic("Tests for articles")
 public class ArticleTests extends CoreTestCase {
 
   @Test
+  @Features(value = {@Feature(value = "Search"), @Feature(value = "Article")})
   @DisplayName("Compare article title with expected one")
   @Description("We search an article about Java and compare title")
   @Step("Starting test testCompareArticleTitle")
+  @Severity(value = SeverityLevel.BLOCKER)
   public void testCompareArticleTitle() {
     SearchPageObject SearchPageObject = SearchPageObjectFactory.get(driver);
 
@@ -28,6 +30,8 @@ public class ArticleTests extends CoreTestCase {
     ArticlePageObject.waitForTitleElement();
     String article_title = ArticlePageObject.getArticleTitle();
 
+    ArticlePageObject.takeScreenshot("article_page");
+
     Assert.assertEquals(
             "Unexpected title",
             "Java (programming language)",
@@ -35,9 +39,11 @@ public class ArticleTests extends CoreTestCase {
   }
 
   @Test
+  @Features(value = {@Feature(value = "Search"), @Feature(value = "Article")})
   @DisplayName("Compare article title with expected v.2")
   @Description("We search an article about Java and compare title not waiting title to appear")
   @Step("Starting test testAssertArticleTitleWithNoWait")
+  @Severity(value = SeverityLevel.CRITICAL)
   public void testAssertArticleTitleWithNoWait() {
     SearchPageObject SearchPageObject = SearchPageObjectFactory.get(driver);
 
@@ -50,9 +56,11 @@ public class ArticleTests extends CoreTestCase {
   }
 
   @Test
+  @Features(value = {@Feature(value = "Search"), @Feature(value = "Article")})
   @DisplayName("Swipe article to the footer")
   @Description("We open an article and swipe it to the footer")
   @Step("Starting test testSwipeArticle")
+  @Severity(value = SeverityLevel.NORMAL)
   public void testSwipeArticle() {
     SearchPageObject SearchPageObject = SearchPageObjectFactory.get(driver);
 
