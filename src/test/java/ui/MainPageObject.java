a81b40f94d5ce84d5eaf59c93e7c09c0c70f6c59
Yaremenko Roman <roman.jaremenko@gmail.com>
2022-05-26 22:26:50 +0300

    Method takeScreenshot() added

--- src/test/java/ui/MainPageObject.java
+++ src/test/java/ui/MainPageObject.java
@@ -5,15 +5,14 @@ import io.appium.java_client.TouchAction;
 import io.appium.java_client.touch.WaitOptions;
 import io.appium.java_client.touch.offset.PointOption;
 import lib.Platform;
+import org.apache.commons.io.FileUtils;
 import org.junit.Assert;
-import org.openqa.selenium.By;
-import org.openqa.selenium.Dimension;
-import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.io.File;
 import java.time.Duration;
 import java.util.List;
 import java.util.regex.Pattern;
@@ -285,6 +284,18 @@ public class MainPageObject {
       }
       ++current_attempts;
     }
+  }
 
+  public String takeScreenshot(String name) {
+    TakesScreenshot ts = (TakesScreenshot) this.driver;
+    File source = ts.getScreenshotAs(OutputType.FILE);
+    String path = System.getProperty("user.dir") + "/" + name + "_screenshot.png";
+    try {
+      FileUtils.copyFile(source, new File(path));
+      System.out.println("The screenshot was taken: " + path);
+    } catch (Exception e) {
+      System.out.println("Cannot take screenshot. Error: " + e.getMessage());
+    }
+    return path;
   }
 }
