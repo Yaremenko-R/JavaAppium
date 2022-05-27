0deda02df31ac4989e947608019ec5c788a7bcd0
Yaremenko Roman <roman.jaremenko@gmail.com>
2022-05-27 17:55:06 +0300

    Allure annotations added

--- src/test/java/ui/AuthPageObject.java
+++ src/test/java/ui/AuthPageObject.java
@@ -1,5 +1,6 @@
 package ui;
 
+import io.qameta.allure.Step;
 import org.openqa.selenium.remote.RemoteWebDriver;
 
 public class AuthPageObject extends MainPageObject {
@@ -13,16 +14,19 @@ public class AuthPageObject extends MainPageObject {
     super(driver);
   }
 
+  @Step("Clicking authorization button")
   public void clickAuthButton() {
     this.waitForElementPresent(LOGIN_BUTTON, "Cannot find login button", 5);
     this.waitForElementAndClick(LOGIN_BUTTON, "Cannot find and click login button", 5);
   }
 
+  @Step("Entering login data")
   public void enterLoginData(String login, String password) {
     this.waitForElementAndSendKeys(LOGIN_INPUT, login, "Cannot find and put a login to the login input ", 5);
     this.waitForElementAndSendKeys(PASSWORD_INPUT, password, "Cannot find and put a password to the password input ", 5);
   }
 
+  @Step("Clicking submit button")
   public void submitForm() {
     this.waitForElementAndClick(SUBMIT_BUTTON, "Cannot find and click submit button", 5);
   }
